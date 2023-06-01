d3d8a41a08ad625e102762c023399d1c4e5bb014
dasRAINBOW <69715079+dasRAINBOW@users.noreply.github.com>
2023-06-01 12:46:45 +0200

    Better bypass

--- crack.py
+++ crack.py
@@ -5,6 +5,7 @@ import numpy
 import pytesseract
 import pyautogui
 from time import sleep
+import requests
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 firstRun = True
@@ -39,11 +40,28 @@ def specialMode():
             # im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
             text = pytesseract.image_to_string(im)
-            # print(text.strip().replace("\n", " "))
-            pyautogui.typewrite(text.strip().replace("\n", " "), 0.014)
+            text = text.strip().replace("\n", " ")
+            splitText = text.split()
+            dele = 0
+            for i in range(len(splitText)):
+                word = splitText[i - dele]
+                if word not in splitContent:
+                    del splitText[i - dele]
+                    dele += 1
+            for e in range(len(splitText)):
+                text += splitText[e] + " "
+            pyautogui.typewrite(text, 0.012)
             with pyautogui.hold('tab'):
                 pyautogui.press('enter')
             exit()
+
+def devMode():
+    print("Nothing to see here :\\")
+    sleep(3)
+        
+with open('english.txt', encoding="utf8") as f:
+    contents = f.read()
+    splitContent = contents.split()
 
 mode = input("Norm = 1; Comp = 2; Bypass = 3; ")
 
@@ -56,3 +74,7 @@ elif mode == str(2):
 elif mode == str(3):
     mon = {'top': 250, 'left': 580, 'width': 600, 'height': 200}
     specialMode()
+elif mode == "das":
+    mon = {'top': 250, 'left': 580, 'width': 600, 'height': 200}
+    print("Dev mode enabled!")
+    devMode()
